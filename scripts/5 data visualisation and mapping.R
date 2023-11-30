0e5bf0f6922400b07a95aafe0479dbd4b2227f05
TinaTiresome <113856966+TinaTiresome@users.noreply.github.com>
2023-11-30 15:51:48 +0100

    Update 5 data visualisation and mapping.R

--- scripts/5 data visualisation and mapping.R
+++ scripts/5 data visualisation and mapping.R
@@ -1,6 +1,6 @@
 # Script: Data Visualisations
 # Author: Tina
-# Date: 29.11.2023
+# Date: 30.11.2023
 
 library(dplyr)     # for data tranformations
 library(tidyr)     # for tidy data
@@ -17,11 +17,12 @@ map_data <- shapefile_data %>%
   left_join(df, by = c("Regions", "year"))
 
 ######### -- Function Defs -- ##################
-# 'mapmaker()' function to create maps for different indicators
+# -- mapmaker -- #
 mapmaker <-
   function(data,
            indicator_variable,
            title_base,
+           color_palette,
            color_limits = NULL) {
     years <- 2014:2018
     
@@ -42,19 +43,12 @@ mapmaker <-
       # Dynamic variable mapping
       fill_aes <- aes_string(fill = indicator_variable)
       
-      # Custom color palette
-      custom_palette <- c("#c2f9b6",
-                           "#868686",
-                           "#5f0a64")
-      
       # Plotting the map
       p <- ggplot(temp_map_data) +
         geom_sf(fill_aes, color = NA) +
-        scale_fill_gradientn(
-          colors = custom_palette, 
-          limits = color_limits, 
-          oob = scales::squish
-          ) +
+        scale_fill_gradientn(colors = color_palette,
+                             limits = color_limits,
+                             oob = scales::squish) +
         labs(title = paste(title_base, year),
              fill = title_base) +
         theme_void() +
@@ -74,82 +68,106 @@ mapmaker <-
     }
   }
 
+# -- scattermaker -- #
+scattermaker <- function(data, x_var, y_var, x_lab, y_lab) {
+  p <- ggplot(data, aes_string(x = x_var, y = y_var)) +
+    geom_point() +
+    ggtitle(paste("Scatter plot of", y_var, "vs.", x_var)) +
+    xlab(x_lab) +
+    ylab(y_lab) +
+    theme_minimal() +
+    theme(
+      plot.background = element_rect(fill = "white", colour = "white"),
+      plot.title = element_text(size = 14)
+    )
+  
+  ggsave(paste0("outputs/graphs/scatter_", x_var, "_vs_", y_var, ".png"), plot = p, width = 10, height = 8)
+}
+
+
+## Custom Colour Palettes
+# Custom color palette
+palette_gradient <- c("#c2f9b6",
+                      "#868686",
+                      "#5f0a64")
+
+
 #################################
 # -- Map Maker Calls -- #
 mapmaker(
-  map_data,
-  "perc_change_Pop_65_to_75_Meds",
-  "Year on Year Change in Share of Population 65-75 on CV Medication"
+  data = map_data,
+  indicator_variable = "perc_change_Pop_65_to_75_Meds",
+  title_base = "Year on Year % Change in Share of Population 65-75 on CV Medication",
+  color_palette = palette_gradient,
 )
 
 mapmaker(
-  map_data,
-  "perc_change_GP_within_5km",
-  "Year on Year Change of GP practises within 5km by Municipality"
+  data = map_data,
+  indicator_variable = "perc_change_GP_within_5km",
+  title_base = "Year on Year Change of GP practises within 5km by Municipality",
+  color_palette = palette_gradient,
 )
-mapmaker(map_data,
-         "avg_Distance_GP",
-         "Average Distance to GP within Municipalities in KM")
 
+mapmaker(
+  data = map_data,
+  indicator_variable = "avg_Distance_GP",
+  title_base = "Average Distance to GP within Municipalities in KM",
+  color_palette = palette_gradient,
+)
 
-###### Scatter Plots
-# Scatter plot for main effect variable meds
-ggplot(df, aes(x = avg_Distance_GP, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle("Scatter plot of avg_Distance_GP vs. perc_Pop_65_to_75_Meds") +
-  xlab("Average Distance to GP") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+# -- Scatter Maker Calls -- #
+scattermaker(
+  df,
+  "perc_change_GP_within_5km",
+  "perc_change_Pop_65_to_75_Meds",
+  "Y-o-Y % Change of number of GPs within 5km of Municipality",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
 
-# Scatter plot for main effect variable meds - now natural logged
-ggplot(df, aes(x = ln_avg_Distance_GP, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle("Scatter plot of LN_avg_Distance_GP vs. perc_Pop_65_to_75_Meds") +
-  xlab("Average Distance to GP") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+scattermaker(
+  df,
+  "perc_Pop_aged_65_up",
+  "perc_change_Pop_65_to_75_Meds",
+  "% of Population aged 65+",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
 
-# perc_Pop_aged_65_up
-ggplot(df, aes(x = perc_Pop_aged_65_up, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle("Scatter plot of perc_Pop_aged_65_up vs. perc_Pop_65_to_75_Meds") +
-  xlab("% of Population aged 65+") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+scattermaker(
+  df,
+  "ln_avg_Standardized_Income",
+  "perc_change_Pop_65_to_75_Meds",
+  "Log of Average Standardized Income",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
 
-#ln avg income
-ggplot(df,
-       aes(x = ln_avg_Standardized_Income, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle("Scatter plot of ln_avg_Standardized_Income vs. perc_Pop_65_to_75_Meds") +
-  xlab("Log of Average Standardized Income") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+scattermaker(
+  df,
+  "predominantly_Rural",
+  "perc_change_Pop_65_to_75_Meds",
+  "Predominantly Rural (binary)",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
 
-# perc_Pop_migration_background
-ggplot(df,
-       aes(x = perc_Pop_migration_background, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle("Scatter plot of perc_Pop_migration_background vs. perc_Pop_65_to_75_Meds") +
-  xlab("% of Population with migration background") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+scattermaker(
+  df,
+  "perc_change_deaths_CVD",
+  "perc_change_Pop_65_to_75_Meds",
+  "Y-o-Y % Change in Deaths attributed to Cardiovascular Disease",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
 
-# perc_Pop_Social_Security_excl_Pensioners
-ggplot(df,
-       aes(x = perc_Pop_Social_Security_excl_Pensioners, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle(
-    "Scatter plot of perc_Pop_Social_Security_excl_Pensioners vs. perc_Pop_65_to_75_Meds"
-  ) +
-  xlab("% of Population on Social Security Benefits (up to Pension age)") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+scattermaker(
+  df,
+  "ln_perc_Pop_migration_background",
+  "perc_change_Pop_65_to_75_Meds",
+  "Log of % of Population with Migration Background",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
 
-# predominantly_Rural
-ggplot(df, aes(x = predominantly_Rural, y = perc_Pop_65_to_75_Meds)) +
-  geom_point() +
-  ggtitle("Scatter plot of predominantly_Rural (binary) vs. perc_Pop_65_to_75_Meds") +
-  xlab("Log of Average Standardized Income") +
-  ylab("Percentage of Population Aged 65 - 75 on Meds") +
-  theme_minimal()
+scattermaker(
+  df,
+  "perc_Pop_Social_Security_excl_Pensioners",
+  "perc_change_Pop_65_to_75_Meds",
+  "% of Population on Social Security Benefits (up to Pension age)",
+  "Percentage of Population Aged 65 - 75 with Cardiovascular Meds"
+)
